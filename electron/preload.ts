6143a1402d3f9501c53c498fe75e5b98c56aeba5
Wai Yan <yanlay129@gmail.com>
2026-07-06 10:50:38 +0000

    WIP

---
+++ electron/preload.ts
@@ -0,0 +1,22 @@
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
+
+type Callback = (...args: any[]) => void;
+
+const sendAllowed = ['toMain'];
+const receiveAllowed = ['fromMain'];
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  sendToMain: (channel: string, data: any) => {
+    if (sendAllowed.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  onFromMain: (channel: string, callback: Callback) => {
+    if (receiveAllowed.includes(channel)) {
+      const handler = (_event: IpcRendererEvent, ...args: any[]) => callback(...args);
+      ipcRenderer.on(channel, handler);
+      return () => ipcRenderer.removeListener(channel, handler);
+    }
+    return () => {};
+  },
+});
